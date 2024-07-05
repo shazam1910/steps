97a2aca62d278e8ac0f7da4a7b982e2cc695a0b1
Shashank Pratheepkumar <Shashanksanthosh19@gmail.com>
2024-07-05 11:41:01 +0530

    Adding event handlers to send alerts

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,18 @@ const messages = [
 
 export default function App() {
     const step =1;
+
+    function eventPrevious(){
+        alert("Previous")
+    }
+
+    
+    function eventNext(){
+        alert("Next")
+    }
+
+
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -23,8 +35,8 @@ export default function App() {
       <p className="message">Step {step}: {messages[step -1]}</p>
 
       <div className="buttons">
-        <button style={{backgroundColor: "#7950f2", color: "#ffff"}}>Previous</button>
-        <button style={{backgroundColor: "#7950f2", color: "#ffff"}}>Next</button>
+        <button style={{backgroundColor: "#7950f2", color: "#ffff"}} onClick = {eventPrevious}>Previous</button>
+        <button style={{backgroundColor: "#7950f2", color: "#ffff"}} onClick = {eventNext}>Next</button>
       </div>
     </div>
   );
