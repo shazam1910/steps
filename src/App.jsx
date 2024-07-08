d4cab7c947d935e2575319efe1e1277343bcbc67
Shashank Pratheepkumar <Shashanksanthosh19@gmail.com>
2024-07-08 12:25:38 +0530

    updating css file with colours

--- src/App.jsx
+++ src/App.jsx
@@ -13,45 +13,46 @@ export default function App() {
   const [isOpen, setIsOpen] = useState(true);
 
   function eventPrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s)=> s-1);
   }
 
   function eventNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s)=> s+1);
   }
 
   return (
     <>
-      <button className="close" onClick={()=> setIsOpen(!isOpen)}>&times;</button>
-{ isOpen && (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+      <button className="close" onClick={() => setIsOpen((is)=> !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
 
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#ffff" }}
-          onClick={eventPrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#ffff" }}
-          onClick={eventNext}
-          >
-          Next
-        </button>
-      </div>
-    </div>
-)
-    }
-          </>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#ffff" }}
+              onClick={eventPrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#ffff" }}
+              onClick={eventNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
